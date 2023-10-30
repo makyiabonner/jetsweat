9d345e433a1da10f42858d8fa847e40cc6962c2b
makyia bonner <makyiabonner@gmail.com>
2023-10-30 14:55:39 -0400

    feat: hero section responsive and stylized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Navbar from "./components/Navbar/Navbar";
+import React from "react";
+import video from "./landing_section.mp4";
 import "react-bootstrap";
 import "./App.css";
 
@@ -6,6 +8,21 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <section className="Hero">
+        <video
+          className="Hero--video"
+          src={video}
+          preload="auto"
+          loop
+          playsInline
+          autoPlay
+          muted
+        />
+        <h1 className="Hero--text">
+          The best boutique fitness classes in the world,
+        </h1>
+        <div className="Hero--transition"></div>
+      </section>
     </div>
   );
 }
